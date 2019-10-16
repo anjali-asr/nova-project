7b9b9eb186b30045a9c48393c215f8650200968e
Anjali Singh <anjali@Anjalis-MacBook-Air.local>
2019-10-17 00:52:22 +0530

    Changes in category business

--- data/src/category/business/category.business.js
+++ data/src/category/business/category.business.js
@@ -11,14 +11,15 @@ exports.addCategory = async (data) => {
 
 //Add products in category
 exports.addProdInCategory = async (data, id) => { //here "id" is category _id
-    let res = await Categories.findByIdAndUpdate(id, { $push: { products: data } }).lean();  //save the product id's in products array
+    let res = await Categories.findByIdAndUpdate(id, { $push: { products: data.products } }, { new: true }).lean();  //save the product id's in products array
     if (!res) msg.notExist;
     return res;
 };
 
 //get list of  by categry name
 exports.productListByCategory = async (id) => {  //here "id" is category _id
-let res = await Categories.findById(id).lean().populate("products");
-if (!res) msg.notExist;
-return res;
+    let res = await Categories.findById(id).lean().populate("products");
+    if (!res) msg.notExist;
+    let result = res.products;
+    return result;
 };
